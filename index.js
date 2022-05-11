6490b8524c1e9c254386a85fb6656ea688a13662
Jasminehy0919 <65463499+Jasminehy0919@users.noreply.github.com>
2022-05-11 11:21:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ $(".download-button").hover(function() {
     $(".title-QR").removeClass("shake");
   },500)
 });
+
+
+
+const cursor =document.querySelector(".cursor");
+
+document.addEventListener('mousemove', e=>{
+  cursor.setAttribute("style","top: "+e.pageY +"px; left:"+e.pageX+"px;")
+})
