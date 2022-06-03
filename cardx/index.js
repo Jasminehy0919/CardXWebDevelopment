c140c3551c56673e4fb0070ab104b7cf4ef63f98
Jasminehy0919 <65463499+Jasminehy0919@users.noreply.github.com>
2022-06-03 00:08:07 -0400

    Add files via upload

--- cardx/index.js
+++ cardx/index.js
@@ -1,6 +1,6 @@
 $(".download-button").hover(function() {
-  $(".title-QR").addClass("shake");
+  $(".title-QR").addClass("shake_effect");
   setTimeout(function(){
-    $(".title-QR").removeClass("shake");
+    $(".title-QR").removeClass("shake_effect");
   },500)
 });
